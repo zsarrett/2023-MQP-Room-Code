8dcc94a5a0fb250bf9145dd8ab3ad874c36f505b
goreilly2001 <goreilly@wpi.edu>
2023-02-17 15:01:46 -0500

    Added pub/sub

--- full game/full game.ino
+++ full game/full game.ino
@@ -158,3 +158,71 @@ void loop() {
 
   //analogWrite (yellowLED, brightness); // Put the value read for the LED
 }
+
+
+
+void pub(const char* topic, char* msg){
+  client.publish(topic, msg);
+}
+void sub(const char* topic){
+  client.subscribe(topic);
+}
+
+void reconnect() {
+  char* recn = "connecting...";
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    display_lcd(recn);
+    if (client.connect(ID)) {
+      //Write ALL Subscribers
+      sub(receive_code);
+      sub(marker_id);
+    } else {
+      WiFi.disconnect();
+      WiFi.reconnect();
+      Serial.println(" try again in 2 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void check_connection(){
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+/* Handles ALL Subscribers */
+void callback(char* topic, byte* message, unsigned int length) {
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    messageTemp += (char)message[i];
+  }
+
+//  Serial.print(messageTemp);
+  
+  if (String(topic) == receive_code) {
+    if(messageTemp == "accepted"){
+      passcodeAccepted = true;
+    }
+    else if(messageTemp == "denied"){
+      passcodeAccepted = false;
+    }
+  }
+
+  if (String(topic) == marker_id) {
+    if(messageTemp == "[1]"){
+      nearCabinet = 1;
+    }
+    else if(messageTemp == "[2]"){
+      nearCabinet = 2;
+    }
+    else if(messageTemp == "[3]"){
+      nearCabinet = 3;
+    }
+  }
+}
