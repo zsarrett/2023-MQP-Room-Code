1cbed810156067407b448d73e6a49ec65b662b07
LiangL2020 <qiuyinglu2012@gmail.com>
2023-04-10 13:38:41 -0400

    auto indented

--- SimonPuzzle/SimonPuzzle.ino
+++ SimonPuzzle/SimonPuzzle.ino
@@ -5,12 +5,12 @@
  */
 #include "WiFi.h"
 #include <PubSubClient.h>
-//#include "analogWrite.h"
+// #include "analogWrite.h"
 
 const int yellowLED = 14;
 const int redLED = 27;
 const int blueLED = 22;
-int brightnessY;  // The value read by the photoresistor
+int brightnessY; // The value read by the photoresistor
 int brightnessR;
 int brightnessB;
 bool redOn;
@@ -23,8 +23,7 @@ const int AIN1 = 36;
 const int AIN2 = 37;
 const int PWMA = 26;
 
-
-// --- MQTT Setup --- 
+// --- MQTT Setup ---
 const int httpPort = 80;
 const char *ssid = "WPI-Open";
 const char *password = NULL;
@@ -32,16 +31,18 @@ const char *ID = "room_mqtt";
 const char *mqtt_server = "mqtt.eclipseprojects.io";
 
 WiFiClient wclient;
-PubSubClient client(wclient); 
+PubSubClient client(wclient);
 
-void wifi_setup() {
+void wifi_setup()
+{
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -53,15 +54,15 @@ void wifi_setup() {
 
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  sub("website/test"); 
+  sub("website/test");
 }
-// --- MQTT Setup Ends --- 
+// --- MQTT Setup Ends ---
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
- // pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
   pinMode(yellowLED, OUTPUT);
   pinMode(redLED, OUTPUT);
   pinMode(blueLED, OUTPUT);
@@ -75,74 +76,88 @@ void setup() {
   yellowOn = false;
   count = 0;
 
-  wifi_setup(); 
+  wifi_setup();
 
-  //Serial.println("Setup done");
+  // Serial.println("Setup done");
 }
 
-bool simonRed() {
-  brightnessR = analogRead(33);    // Read the brightness
-  brightnessR = brightnessR / 16;  // Adjust the brightness value
+bool simonRed()
+{
+  brightnessR = analogRead(33);   // Read the brightness
+  brightnessR = brightnessR / 16; // Adjust the brightness value
   Serial.print("Red");
   Serial.println(brightnessR);
-  if (brightnessR < 200 && redOn == false) {
-   // Serial.println("Flashing RED");
-    digitalWrite(redLED, LOW);  // turn off light
+  if (brightnessR < 200 && redOn == false)
+  {
+    // Serial.println("Flashing RED");
+    digitalWrite(redLED, LOW); // turn off light
     delay(500);
-    digitalWrite(redLED, HIGH);  // turn on the LED
-    delay(500);                  // wait for half a second or 500 milliseconds
-    digitalWrite(redLED, LOW);   // turn off the LED
-    delay(500);                  // wait for half a second or 500 milliseconds
-  } else {
-    digitalWrite(redLED, HIGH);  // turn on light
-    delay(10);                   // Wait 10 ms//digitalWrite (ledPin2, HIGH);
-    //Serial.println("RED FOUND");
+    digitalWrite(redLED, HIGH); // turn on the LED
+    delay(500);                 // wait for half a second or 500 milliseconds
+    digitalWrite(redLED, LOW);  // turn off the LED
+    delay(500);                 // wait for half a second or 500 milliseconds
+  }
+  else
+  {
+    digitalWrite(redLED, HIGH); // turn on light
+    delay(10);                  // Wait 10 ms//digitalWrite (ledPin2, HIGH);
+    // Serial.println("RED FOUND");
     redOn = true;
     return true;
   }
   return false;
 }
 
-bool simonBlue() {
-  if (redOn == true && blueOn == false) {
+bool simonBlue()
+{
+  if (redOn == true && blueOn == false)
+  {
     brightnessB = analogRead(34);
     brightnessB = brightnessB / 16;
-    Serial.println("Brightness B: "  + brightnessB);
-    if (brightnessB < 200) {
-      //Serial.println("Flash BLUE");
-      digitalWrite(blueLED, LOW);  // turn off light
+    Serial.println("Brightness B: " + brightnessB);
+    if (brightnessB < 200)
+    {
+      // Serial.println("Flash BLUE");
+      digitalWrite(blueLED, LOW); // turn off light
       delay(500);
-      digitalWrite(blueLED, HIGH);  // turn on the LED
-      delay(500);                   // wait for half a second or 500milliseconds
-      digitalWrite(blueLED, LOW);   // turn off the LED
-      delay(500);                   // wait for half a second or 500milliseconds
-    } else {
+      digitalWrite(blueLED, HIGH); // turn on the LED
+      delay(500);                  // wait for half a second or 500milliseconds
+      digitalWrite(blueLED, LOW);  // turn off the LED
+      delay(500);                  // wait for half a second or 500milliseconds
+    }
+    else
+    {
       digitalWrite(blueLED, HIGH);
       delay(10);
-      //Serial.println("BLUE FOUND");
+      // Serial.println("BLUE FOUND");
       blueOn = true;
       return true;
     }
   }
-  return false;  
+  return false;
 }
 
-bool simonYellow() {
-  if (blueOn == true && yellowOn == false) {
+bool simonYellow()
+{
+  if (blueOn == true && yellowOn == false)
+  {
     brightnessY = analogRead(35);
     brightnessY = brightnessY / 16;
     Serial.print("Brightness Y: ");
-    //Serial.println(brightnessY);
-    if (brightnessY < 200) {
-      //Serial.println("YELLOW FLASH");
-      digitalWrite(yellowLED, LOW);  // turn off light
+    // Serial.println(brightnessY);
+    if (brightnessY < 200)
+    {
+      // Serial.println("YELLOW FLASH");
+      digitalWrite(yellowLED, LOW); // turn off light
       delay(500);
-      digitalWrite(yellowLED, HIGH);  // turn on the LED
-      delay(500);                     // wait for half a second or 500 milliseconds
-      digitalWrite(yellowLED, LOW);   // turn off the LED
-      delay(500);                     // wait for half a second or 500 milliseconds
-    } else {
-      //Serial.println("FOUND YELLOW");
+      digitalWrite(yellowLED, HIGH); // turn on the LED
+      delay(500);                    // wait for half a second or 500 milliseconds
+      digitalWrite(yellowLED, LOW);  // turn off the LED
+      delay(500);                    // wait for half a second or 500 milliseconds
+    }
+    else
+    {
+      // Serial.println("FOUND YELLOW");
       digitalWrite(yellowLED, HIGH);
       delay(10);
       yellowOn = true;
@@ -152,26 +167,34 @@ bool simonYellow() {
   return false;
 }
 
-bool simon() {
-  //Serial.println("START RED");
-  if (redOn == false){simonRed();}
-  //Serial.println("END RED");
-  //Serial.println("START BLUE");
-  if (blueOn == false){simonBlue();}
-  //Serial.println("END BLUE");
- // Serial.println("START YELLOW");
-  if (yellowOn == false){simonYellow();}
-  //Serial.println("END YELLOW");
+bool simon()
+{
+  // Serial.println("START RED");
+  if (redOn == false)
+  {
+    simonRed();
+  }
+  // Serial.println("END RED");
+  // Serial.println("START BLUE");
+  if (blueOn == false)
+  {
+    simonBlue();
+  }
+  // Serial.println("END BLUE");
+  // Serial.println("START YELLOW");
+  if (yellowOn == false)
+  {
+    simonYellow();
+  }
+  // Serial.println("END YELLOW");
 
-  //door();
+  // door();
 
   return true;
 }
 
-//if blueOn == false and redOn == true
-// void door(){
-
-
+// if blueOn == false and redOn == true
+//  void door(){
 
 //   if(yellowOn== true && count == 0){
 //    //drive motor forward (positive speed)
@@ -195,37 +218,45 @@ bool simon() {
 //   }
 // }
 
-//if blueOn == false and redOn == true`x
+// if blueOn == false and redOn == true`x
 
-void loop() {
-  //Serial.println("HIHI");
-  pub("room/puzzle", "1"); 
-  check_connection(); 
+void loop()
+{
+  // Serial.println("HIHI");
+  pub("room/puzzle", "1");
+  check_connection();
   simon();
-  pub("room/puzzle", "2"); 
+  pub("room/puzzle", "2");
   delay(100);
-  //door();
+  // door();
 
-  //analogWrite (yellowLED, brightness); // Put the value read for the LED
+  // analogWrite (yellowLED, brightness); // Put the value read for the LED
 }
 
 /************************ MQTT Methods ************************/
-void pub(const char* topic, char* msg){
+void pub(const char *topic, char *msg)
+{
   client.publish(topic, msg);
 }
-void sub(const char* topic){
+void sub(const char *topic)
+{
   client.subscribe(topic);
 }
 
-void reconnect() {
-  char* recn = "connecting...";
-  while (!client.connected()) {
+void reconnect()
+{
+  char *recn = "connecting...";
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(ID)) {
-      //Write ALL Subscribers
-      sub("website/test"); 
-    } else {
+    if (client.connect(ID))
+    {
+      // Write ALL Subscribers
+      sub("website/test");
+    }
+    else
+    {
       WiFi.disconnect();
       WiFi.reconnect();
       Serial.println(" try again in 5 seconds");
@@ -235,22 +266,25 @@ void reconnect() {
   }
 }
 
-void check_connection(){
-  if (!client.connected()) {
+void check_connection()
+{
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
 }
 
 /* Handles ALL Subscribers */
-void callback(char* topic, byte* message, unsigned int length) {
+void callback(char *topic, byte *message, unsigned int length)
+{
   String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
+
+  for (int i = 0; i < length; i++)
+  {
     messageTemp += (char)message[i];
   }
 
   Serial.println(messageTemp);
-
 }
 /*************************************************************/
